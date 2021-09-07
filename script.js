b0b3344bfe951da94bfb3ec0c059fff375075304
bleurpy <raphael.colleter@gmail.com>
2021-09-07 15:58:23 +0300

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+/*var animationbtn = document.getElementById('btn_home')
+animationbtn.addEventListener("mouseover",function(){
+
+    animationbtn.style.backgroundColor = "#2cc1ff";
+    animationbtn.style.top = "5";
+    
+});
+
+animationbtn.addEventListener("mouseleave",function(){
+
+    animationbtn.style.backgroundColor = "#A0EE99";
+    animationbtn.style.top = "0";
+    
+});*/
