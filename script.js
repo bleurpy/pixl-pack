7397b6966074751839733339300b97e0f2b56155
bleurpy <78358351+bleurpy@users.noreply.github.com>
2021-10-09 18:51:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ animationbtn.addEventListener("mouseleave",function(){
     animationbtn.style.top = "0";
     
 });*/
+const titreSpans = document.querySelectorAll('h1');
+
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL
+    .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    TL.play();
+})
